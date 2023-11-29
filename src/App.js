315c57866729baeaa9b535d7f70e3e8a5b2783be
muavia haidri <muavia00842@gmail.com>
2023-11-29 23:09:31 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import styles from "styles/App.module.scss";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import clsx from "clsx";
+import styles from 'styles/App.module.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import clsx from 'clsx';
 
 // PAGES
-import Home from "pages/Home";
-import Detail from "pages/Detail";
-import Category from "pages/Category";
+import Home from 'pages/Home';
+import Detail from 'pages/Detail';
+import Category from 'pages/Category';
 
 // COMPONENTS
-import Header from "components/Header";
-import BasketSidebar from "components/BasketSidebar";
-import Footer from "components/Footer";
-import MobileBottomNav from "components/MobileBottomNav";
+import Header from 'components/Header';
+import BasketSidebar from 'components/BasketSidebar';
+import MobileBottomNav from 'components/MobileBottomNav';
 
 // HOOKS
-import useMobileDetect from "hooks/useMobileDetect";
+import useMobileDetect from 'hooks/useMobileDetect';
 
 // CONTEXT
-import BasketContextProvider from "context/BasketContext";
+import BasketContextProvider from 'context/BasketContext';
 
 const App = () => {
   const device = useMobileDetect();
@@ -25,7 +24,12 @@ const App = () => {
   return (
     <Router>
       <BasketContextProvider>
-        <div className={clsx(device.type === "mobile" && styles.paddingForMobile, styles.container)}>
+        <div
+          className={clsx(
+            device.type === 'mobile' && styles.paddingForMobile,
+            styles.container
+          )}
+        >
           <Header />
           <main className={styles.main}>
             <Switch>
@@ -40,10 +44,9 @@ const App = () => {
               </Route>
             </Switch>
           </main>
-          <Footer />
         </div>
         <BasketSidebar />
-        {device.type === "mobile" && <MobileBottomNav />}
+        {device.type === 'mobile' && <MobileBottomNav />}
       </BasketContextProvider>
     </Router>
   );
